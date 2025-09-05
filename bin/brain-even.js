e6075a75650e8f351176cac6ebadbe1488a50718
JuliaLen-web <j.ibatullina@redneckstudio.com>
2025-09-05 17:38:01 +0400

    test fix error v3

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -3,7 +3,7 @@
 import commonLogic from '../src/index.js'
 import { isEven, randomNum } from '../src/utility.js'
 
-let conditionText = `Answer 'yes' if the number is even, otherwise answer 'no'.`
+let conditionText = 'Answer \'yes\' if the number is even, otherwise answer \'no\'.'
 
 function questionFn() {
   return randomNum(100)
--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -3,7 +3,7 @@
 import commonLogic from '../src/index.js'
 import { isPrime, randomNum } from '../src/utility.js'
 
-let conditionText = `Answer 'yes' if given number is prime. Otherwise answer 'no'.`
+let conditionText = 'Answer \'yes\' if given number is prime. Otherwise answer \'no\'.'
 
 function questionFn() {
   return randomNum(100)
